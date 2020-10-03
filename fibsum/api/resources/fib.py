c8390d38358122a5df89305beed9f545d1efda06
amir <amirkhakshour@gmail.com>
2020-10-03 13:40:03 +0200

    connected get verbof FibSumGenerator endpoint to all_comb_sum algorithm

--- fibsum/api/resources/fib.py
+++ fibsum/api/resources/fib.py
@@ -1,6 +1,27 @@
 from flask_restful import Resource
+from fibsum.services.fibsum import all_comb_sum
 
 
 class FibSumGenerator(Resource):
+    """
+    Sum Fibonacci series endpoint:
+
+    ---
+    get:
+      tags:
+        - api
+      parameters:
+        - in: path
+          name: target
+          schema:
+            type: integer
+      responses:
+        200:
+          content:
+            application/json:
+              schema:
+                type: list
+    """
+
     def get(self, target):
-        pass
+        return all_comb_sum(target)
