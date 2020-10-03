13668e147cc87d22368e8f5377ea20b76cd4fb78
amir <amirkhakshour@gmail.com>
2020-10-03 15:59:58 +0200

    added more checkers, redis, db and external API!

--- fibsum/services/chekers.py
+++ fibsum/services/chekers.py
@@ -11,3 +11,21 @@ def dummy_checker():
 def dummy_checker_fails():
     """Check dummy service fails."""
     return False, "Failed"
+
+
+@health_checker.register()
+def check_db_available():
+    """check if DB is available."""
+    return True, "DB is OK"
+
+
+@health_checker.register()
+def check_redis_available():
+    """Use redis client and check if connection to redis is successful."""
+    return True, "Redis is OK"
+
+
+@health_checker.register()
+def check_external_api_one_available():
+    """Check external API #1 connectivity!"""
+    return False, "External API Failed!"
